a005ff82de1549e9e98f88533dc3d52648aa9d3b
AppFreiOnline <sistemas.freionline@acaonsfatima.org.br>
2025-04-21 23:30:38 -0300

    .

--- src/rotas.js
+++ src/rotas.js
@@ -1,14 +1,14 @@
 import acompanhamentoController from './controller/acompanhamentoController.js'
+import preInscricaoController from './controller/preInscricaoController.js'
 import cadastroController from './controller/cadastroController.js'
 import cursosController from './controller/cursosController.js'
 import faqController from './controller/faqController.js'
-import preInscricaoController from './controller/preInscricaoController.js'
 
 export default function adicionarRotas(servidor) {
 
     servidor.use(acompanhamentoController)
+    servidor.use(preInscricaoController)
     servidor.use(cadastroController)
     servidor.use(cursosController)
     servidor.use(faqController)
-    servidor.use(preInscricaoController)
 }
